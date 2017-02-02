1836580ef11891d2fda8f8649c785720da4fa7f8
Jan Medlock <jan.medlock@oregonstate.edu>
2017-02-01 19:49:52 -0600

    Clean up axes formatting.

--- sensitivity_2params.py
+++ sensitivity_2params.py
@@ -48,13 +48,42 @@ def build():
     return rel_growth_rate
 
 
+def _hide_ticklabels(ax):
+    for l in ax.get_ticklabels():
+        l.set_visible(False)
+    ax.offsetText.set_visible(False)
+
+
+def _format_axis(ax, xlabel, ylabel):
+    for axis in (ax.xaxis, ax.yaxis):
+        axis.set_major_formatter(ticker.StrMethodFormatter('{x:g}'))
+        scale = axis.get_scale()
+        if scale == 'linear':
+            axis.set_major_locator(ticker.MaxNLocator(nbins = 4))
+        elif scale == 'log':
+            axis.set_major_locator(ticker.LogLocator(subs = [1, 2, 5]))
+            axis.set_minor_locator(ticker.NullLocator())
+
+    ax.tick_params(labelsize = 9)
+
+    if ax.is_last_row():
+        ax.set_xlabel(xlabel, fontsize = 9)
+    else:
+        _hide_ticklabels(ax.xaxis)
+
+    if ax.is_first_col():
+        ax.set_ylabel(ylabel, fontsize = 9)
+    else:
+        _hide_ticklabels(ax.yaxis)
+
+
 def plot(rel_growth_rate):
     # Contour levels every log10.
     log_rgr = numpy.log10(rel_growth_rate)
     log_rgr_absmax = numpy.max(numpy.abs(log_rgr))
     log_rgr_levels = numpy.linspace(- numpy.ceil(log_rgr_absmax),
                                     numpy.ceil(log_rgr_absmax),
-                                    2 * numpy.ceil(log_rgr_absmax) + 1)
+                                    2 * int(numpy.ceil(log_rgr_absmax)) + 1)
     contour_levels = 10 ** log_rgr_levels
 
     colors = seaborn.color_palette()
@@ -64,7 +93,7 @@ def plot(rel_growth_rate):
     ncols = nparams - 1
     gs = gridspec.GridSpec(nrows, ncols)
     fig = pyplot.figure(figsize = figsize)
-    for row in range(nparams - 1):
+    for row in range(nrows):
         for col in range(row + 1):
             param0, param0_name = common.sensitivity_parameters[row + 1]
             param1, param1_name = common.sensitivity_parameters[col]
@@ -75,6 +104,8 @@ def plot(rel_growth_rate):
             ax = fig.add_subplot(gs[row, col],
                                  xscale = xscale,
                                  yscale = yscale)
+
+            _format_axis(ax, param0_name, param1_name)
 
             for (k, x) in enumerate(parameters.parameter_sets.items()):
                 n, p = x
@@ -99,32 +130,6 @@ def plot(rel_growth_rate):
                 ax.axhline(param0baseline, linestyle = 'dotted',
                            color = 'black', alpha = alpha)
 
-                if ax.is_last_row():
-                    ax.set_xlabel(param1_name, fontsize = 9)
-                else:
-                    for l in ax.xaxis.get_ticklabels():
-                        l.set_visible(False)
-                    ax.xaxis.offsetText.set_visible(False)
-                if ax.is_first_col():
-                    ax.set_ylabel(param0_name, fontsize = 9)
-                else:
-                    for l in ax.yaxis.get_ticklabels():
-                        l.set_visible(False)
-                    ax.yaxis.offsetText.set_visible(False)
-
-                for axis in (ax.xaxis, ax.yaxis):
-                    scale = axis.get_scale()
-                    if scale == 'linear':
-                        axis.set_major_locator(ticker.MaxNLocator(nbins = 4))
-                    elif scale == 'log':
-                        axis.set_major_locator(ticker.LogLocator(
-                            subs = [1, 2, 5]))
-                    axis.set_major_formatter(ticker.StrMethodFormatter(
-                        '{x:g}'))
-                    ax.tick_params(labelsize = 9)
-
-    fig.tight_layout()
-
     handles = (lines.Line2D([], [], color = c, alpha = alpha)
                for c in seaborn.color_palette())
     labels = parameters.parameter_sets.keys()
@@ -132,13 +137,16 @@ def plot(rel_growth_rate):
                      loc = 'upper right',
                      frameon = False,
                      fontsize = 'medium')
+
+    fig.tight_layout()
+
     if save:
         common.savefig(fig)
 
 
 if __name__ == '__main__':
-    rel_growth_rate = build()
-    numpy.save('sensitivity_2params.npy', rel_growth_rate)
-    # rel_growth_rate = numpy.load('sensitivity_2params.npy')
-    # plot(rel_growth_rate)
-    # pyplot.show()
+    # rel_growth_rate = build()
+    # numpy.save('sensitivity_2params.npy', rel_growth_rate)
+    rel_growth_rate = numpy.load('sensitivity_2params.npy')
+    plot(rel_growth_rate)
+    pyplot.show()
