a8355c64e0ced949224edddb9e3f44d3eda8b666
Jan Medlock <jan.medlock@oregonstate.edu>
2018-03-22 21:57:41 -0400

    Split computing and plotting.

--- growth_rates.py
+++ growth_rates.py
@@ -26,27 +26,29 @@ def get_growth_rate(p, t = common.tmax):
     return r
 
 
-def get_pop_and_growth_rates(p, V0 = 0.1, t = 1000, Vmax = 1000):
-    V0_old = p.V0
-    p.V0 = V0
-    DFS0 = odes.get_DFS0(p)
-    p.V0 = V0_old
-    T = numpy.linspace(0, t, 1001)
-    Y = odes.solve(DFS0, T, p)
-    pop = Y.iloc[:, : -2].sum(1)
-    r, _ = zip(*(odes.get_r_v_Jacobian(t, row[1], p)
-                 for (t, row) in zip(T, Y.iterrows())))
-    ix = (pop <= Vmax)
-    pop_ = numpy.hstack((0, pop[ix], Vmax))
-    r_ = numpy.hstack((0, numpy.asarray(r)[ix], numpy.interp(Vmax, pop, r)))
-    return (pop_, r_)
-
-
-def main():
-    fig, axes = pyplot.subplots(1, 1, figsize = figsize)
+def get_pop_and_growth_rates(V0 = 0.1, t = 1000, Vmax = 1000):
+    pop = {}
+    r = {}
     for (n, p) in parameters.parameter_sets.items():
-        pop, r = get_pop_and_growth_rates(p)
-        axes.plot(pop, r, label = n, alpha = common.alpha)
+        V0_old = p.V0
+        p.V0 = V0
+        DFS0 = odes.get_DFS0(p)
+        p.V0 = V0_old
+        T = numpy.linspace(0, t, 1001)
+        Y = odes.solve(DFS0, T, p)
+        pop_ = Y.iloc[:, : -2].sum(1)
+        r_, _ = zip(*(odes.get_r_v_Jacobian(t, row[1], p)
+                      for (t, row) in zip(T, Y.iterrows())))
+        ix = (pop_ <= Vmax)
+        pop[n] = numpy.hstack((0, pop_[ix], Vmax))
+        r[n] = numpy.hstack((0, numpy.asarray(r_)[ix], numpy.interp(Vmax, pop_, r_)))
+    return (pop, r)
+
+
+def plot(pop, r):
+    fig, axes = pyplot.subplots(1, 1, figsize = figsize)
+    for n in parameters.parameter_sets.keys():
+        axes.plot(pop[n], r[n], label = n, alpha = common.alpha)
     axes.set_xlabel('Initial vector population size')
     axes.set_ylabel('Pathogen intrinsic growth rate (d$^{-1}$)')
     axes.legend(loc = 'upper left')
@@ -60,5 +62,6 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    pop, r = get_pop_and_growth_rates()
+    plot(pop, r)
     pyplot.show()
